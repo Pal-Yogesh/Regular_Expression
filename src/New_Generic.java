accc56d190f8ed5c6805a1de449ec38466ba3f1c
Pal-Yogesh <yogeshpal5049@gmail.com>
2021-12-17 06:49:53 +0530

    Example_Generic

--- src/New_Generic.java
+++ src/New_Generic.java
@@ -1,21 +1,19 @@
-//package com.GenericClass;
-//
-//public class New_Generic <T>{
-//    T num;
-//    public void set(T value){
-//        System.out.println("T has instantiated at this step");
-//        num = value;
-//    }
-//    public T get(){
-//        return (num);
-//    }
-//}
-//class New{
-//    public static void main(String[] args) {
-//       Test<Integer> t = new Test<Integer>();
-//       t.set(5);
-//       Test<Character> t1 = new Test<Character>();
-//       t1.set('M');
-//        System.out.println("value are: "+t.get()+" "+t1.get());
-//    }
-//}
+public class New_Generic <T>{
+    T num;
+    public void set(T value){
+        System.out.println("T has instantiated at this step");
+        num = value;
+    }
+    public T get(){
+        return (num);
+    }
+}
+class New{
+    public static void main(String[] args) {
+       New_Generic<Integer> t = new New_Generic<Integer>();
+       t.set(5);
+       New_Generic<Character> t1 = new New_Generic<Character>();
+       t1.set('M');
+        System.out.println("value are: "+t.get()+" "+t1.get());
+    }
+}
